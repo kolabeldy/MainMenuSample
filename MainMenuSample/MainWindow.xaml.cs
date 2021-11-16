7be06fb443653c167af28f607f968b88b7143291
Власов Андрей <avvlasov@outlook.com>
2021-11-16 10:27:41 +0300

    Добавьте файлы проекта.

---
+++ MainMenuSample/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MainMenuSample;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        MainWindowViewModel model = new();
+        DataContext = model;
+        InitializeComponent();
+    }
+}
---
+++ MainMenuSample/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using MyServicesLibrary.SideMenu;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace MainMenuSample;
+public class MainWindowViewModel : BaseViewModel
+{
+    public SideMenu? SideMenu { get; set; }
+
+
+    private UserControl _ContentPanel = new();
+    public UserControl ContentPanel
+    {
+        get => _ContentPanel;
+        set
+        {
+            Set(ref _ContentPanel, value);
+        }
+    }
+
+    public MainWindowViewModel()
+    {
+        SideMenu = new SideMenu(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "sidemenu.db"));
+        SideMenu.OnMenuChoise += MainMenuChoiseCommand;
+    }
+
+    private void MainMenuChoiseCommand(string methodName)
+    {
+        if (methodName != null)
+        {
+            MethodInfo? method = typeof(MainWindowViewModel).GetMethod(methodName);
+            if (method != null) method.Invoke(this, null);
+        }
+
+    }
+    //public void AnalysisForCCShow() => mw.MainMenuPanel.Content = AnalysisForCC.GetInstance();
+    public void AnalysisForCCShow() => ContentPanel = new AnalysisForCC();
+    public void AboutShow() => new About().ShowDialog();
+    //public void SettingsShow() => new Setting().ShowDialog();
+
+}
